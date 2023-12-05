c519fb4b3d8a8a059ec54ebbce3e7f42d129aa66
saurabh <agarwal@cs.wisc.edu>
2023-12-05 13:37:24 -0600

    Added new enumerator

---
+++ applications/blox_enumerator.py
@@ -0,0 +1,21 @@
+# grpc
+import grpc_client_blox_iterator as bloxComm
+
+
+class bloxEnumerate(enumerate):
+    def __init__(self, x, start=0):
+        self.x_ = x
+        self.len_ = len(x)
+        self._rpc_client = bloxComm.BloxIterator()
+
+    def __next__(self):
+        # check lease
+        out = super().__next__()
+        status = self._rpc_client.check_lease()
+        return out, status
+
+
+if __name__ == "__main__":
+    # Example usage:
+    for ictr, key in bloxEnumerate(["a", "b", "c"]):
+        pass
