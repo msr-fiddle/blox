3bb113ff96a700c4a88b2444bbc13e141a455412
saurabh <agarwal@cs.wisc.edu>
2023-12-05 19:42:58 -0600

    Fixed additional changes

--- applications/testing_application.py
+++ applications/testing_application.py
@@ -1,6 +1,6 @@
 import sys
 from blox_enumerator import bloxEnumerate
 
-for ictr, key in enumerate(range(sys.argv[1])):
+for ictr, key in enumerate(range(int(sys.argv[1]))):
     print(ict, key)
     time.sleep(10)
