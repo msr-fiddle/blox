f1831b2799b8c3c685ee3c299c2df5eeb0e8b42f
saurabh <agarwal@cs.wisc.edu>
2023-12-13 11:13:51 -0600

    Added previous metric data

--- blox/deployment/grpc_client_rm.py
+++ blox/deployment/grpc_client_rm.py
@@ -129,6 +129,7 @@ class ResourceManagerComm(object):
                     response = stub.GetMetrics(metric_request)
                 metric_data = json.loads(response.response)
                 # make sure we update and not overwrite
+                print("Recieved Metric Data {}".format(metric_data))
                 previous_metric = active_job_dict[job_id]["tracked_metrics"]
                 for key in metric_data:
                     if key == "attained_service":
