53ebd08a297799f3d508492873051481511e7c1b
saurabh <agarwal@cs.wisc.edu>
2024-02-07 22:43:54 -0600

    Added code for command launch

--- command_launch_file.py
+++ command_launch_file.py
@@ -5,4 +5,11 @@ while True:
     with open("job_file_db.txt", "r") as fopen:
         data = fopen.read()
         print(data)
+        proc = subprocess.Popen(
+            f"{command_to_run}  {' '.join(str(i) for i in launch_params)}  2>&1 | tee /dev/shm/job_{job_id}_local_gpu_{local_gpu_id}.log",
+            stdout=subprocess.PIPE,
+            # stderr=subprocess.STDOUT,
+            start_new_session=True,
+            shell=True,
+        )
     time.sleep(10)
