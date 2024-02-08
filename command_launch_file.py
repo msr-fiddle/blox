349c8fff7b4d9dff867862fd0e58dc83f49cf416
saurabh <agarwal@cs.wisc.edu>
2024-02-07 22:44:30 -0600

    Add launch

--- command_launch_file.py
+++ command_launch_file.py
@@ -6,7 +6,7 @@ while True:
         data = fopen.read()
         print(data)
         proc = subprocess.Popen(
-            f"{command_to_run}  {' '.join(str(i) for i in launch_params)}  2>&1 | tee /dev/shm/job_{job_id}_local_gpu_{local_gpu_id}.log",
+            data,
             stdout=subprocess.PIPE,
             # stderr=subprocess.STDOUT,
             start_new_session=True,
