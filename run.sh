95a80b1484d03d8e0bb6fc212b4843800b82d1be
xyu384 <xyu384@wisc.edu>
2024-04-29 16:57:10 -0500

    run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+conda activate pollux
+rm -f out_sim.txt
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python simulator_simple.py --cluster-job-log workload_pollux/workload-6.csv --sim-type trace-synthetic --jobs-per-hour 1 --exp-prefix test > out_sim.txt
+
+conda activate pollux
+rm -f out_sch.txt
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python pollux_scheduler.py --simulate --load 1 --exp-prefix test > out_sch.txt
+
